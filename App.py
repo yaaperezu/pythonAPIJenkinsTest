708fac97ce4e14f79760fd34bee41c851f1063d5
Yasser Pérez <yasserperez10@gmail.com>
2020-10-16 08:55:32 -0500

    Update App.py

--- App.py
+++ App.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def base():
-    return Response(response=json.dumps({"Status": "UP YasserP"}),
+    return Response(response=json.dumps({"Status": "UP Yaaperez"}),
                     status=200,
                     mimetype='application/json')
   
